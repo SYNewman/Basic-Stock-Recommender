7d62a580524bfd441a96915e0d5866edb503fe75
SYNewman <shmuelnewman@hotmail.com>
2024-11-08 14:45:15 +0000

    completed for loop

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from stocks import *
 #etc.
 
 for i in list_of_stocks:
-    print(i)
-    ticker = yfinance.Ticker(i)
+    ticker = yf.Ticker(i)
     current_price = ticker.info["regularMarketPrice"]
     high_price = ticker.info["regularMarketDayHigh"]
     low_price = ticker.info["regularMarketDayLow"]
     open_price = ticker.info["regularMarketOpen"]
     close_price = ticker.info["regularMarketPreviousClose"]
     volume = ticker.info["regularMarketVolume"]
-    print(current_price, high_price, low_price, open_price, close_price, volume)
+    
+    #strategy1.strategy1(current_price, high_price, low_price, open_price, close_price, volume)
+    #strategy2.strategy2(current_price, high_price, low_price, open_price, close_price, volume)
+    #etc.
