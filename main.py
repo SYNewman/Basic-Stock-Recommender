d926f52a3139571c5f2cb7b3acba589b06a3e65c
SYNewman <shmuelnewman@hotmail.com>
2024-11-08 14:54:26 +0000

    added a 'next steps' section to main.py

--- main.py
+++ main.py
@@ -16,3 +16,23 @@ for i in list_of_stocks:
     #strategy1.strategy1(current_price, high_price, low_price, open_price, close_price, volume)
     #strategy2.strategy2(current_price, high_price, low_price, open_price, close_price, volume)
     #etc.
+    
+'''
+next steps:
+- main app
+    - add more stocks
+    - add strategies
+    - edit for loop so that it actually gets the data for each stock
+    - make sure that this data is relevant for the strategies being implemented
+- List of strategies to implement:
+    - strategy1
+    - strategy2
+    - etc.
+- test
+- make more advanced
+    - add oop
+    - add data structures
+- put together with the django app
+    - put data in database and do other jobs relevant for the job
+    - during this stage, keep adding strategies etc. in this app
+'''
