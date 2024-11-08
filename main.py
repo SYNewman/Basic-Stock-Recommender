abcbbb5687675ec4ba68fa169af3f7bcb0a044f6
SYNewman <shmuelnewman@hotmail.com>
2024-11-08 11:37:14 +0000

    created stock_lists.py and made experimental changes

---
+++ main.py
@@ -0,0 +1,12 @@
+#import yfinance
+import stock_lists
+#import strategy1
+#import strategy2
+#etc.
+
+#i need to loop through every list in stock_lists.py
+#for each list i need to loop through each item in the list
+
+for list in stock_lists:
+    for item in list:
+        print(item)
