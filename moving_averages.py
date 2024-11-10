afd7b262230cb65d3fa38a34df1cbd924fc569fd
SYNewman <shmuelnewman@hotmail.com>
2024-11-10 13:40:58 +0000

    removes stocks from opposite list if buy/sell signal triggered

--- moving_averages.py
+++ moving_averages.py
@@ -3,8 +3,12 @@ def moving_averages(bought_stocks, sold_stocks, ticker, price, short_ma, long_ma
         if price > short_ma:
             print(ticker, "BUY (moving averages)")
             bought_stocks.append(ticker)
+            if ticker in sold_stocks:
+                sold_stocks.remove(ticker)
     
     elif short_ma < long_ma:
         if price < short_ma:
             print(ticker, "SELL (moving averages)")
             sold_stocks.append(ticker)
+            if ticker in bought_stocks:
+                bought_stocks.remove(ticker)
