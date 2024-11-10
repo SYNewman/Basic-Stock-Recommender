0cc9b884559fc2c6a09b72909efe06b8e3b6273b
SYNewman <shmuelnewman@hotmail.com>
2024-11-10 13:33:53 +0000

    first part of moving averages complete

--- moving_averages.py
+++ moving_averages.py
@@ -1,2 +1,8 @@
-def moving_averages(price, short_ma, long_ma):
-    pass
+def moving_averages(ticker, price, short_ma, long_ma):
+    if short_ma > long_ma:
+        if price > short_ma:
+            print(ticker, "BUY (moving averages)")
+    
+    elif short_ma < long_ma:
+        if price < short_ma:
+            print(ticker, "SELL (moving averages)")
