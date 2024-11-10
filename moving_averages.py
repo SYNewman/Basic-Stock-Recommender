0555b518178eb5508cbaee53e109d2ee2b321a53
SYNewman <shmuelnewman@hotmail.com>
2024-11-10 19:15:30 +0000

    added spaces in print statements

--- moving_averages.py
+++ moving_averages.py
@@ -4,7 +4,7 @@ def moving_averages(bought_stocks, sold_stocks, ticker, price, short_ma, long_ma
     # Code to generate a buy signal
     if short_ma > long_ma:
         if price > short_ma:
-            print(f"BUY {ticker} (Moving Averages)")
+            print(f"BUY {ticker}   (Moving Averages)")
             # Adds the stock to the list of bought stocks
             bought_stocks.append(ticker)
             # Removes the stock from the list of sold stocks if is in it
@@ -14,7 +14,7 @@ def moving_averages(bought_stocks, sold_stocks, ticker, price, short_ma, long_ma
     # Code to generate a sell signal
     elif short_ma < long_ma:
         if price < short_ma:
-            print(f"SELL {ticker} (Moving Averages)")
+            print(f"SELL {ticker}   (Moving Averages)")
             sold_stocks.append(ticker)
             if ticker in bought_stocks:
                 bought_stocks.remove(ticker)
@@ -22,6 +22,6 @@ def moving_averages(bought_stocks, sold_stocks, ticker, price, short_ma, long_ma
     # Code to check if it is time to sell a stock which was previously bought
     if ticker in bought_stocks:
         if price < short_ma or short_ma < long_ma:
-            print(f"SELL {ticker} (Moving Averages)")
+            print(f"SELL {ticker}   (Moving Averages)")
             sold_stocks.append(ticker)
             bought_stocks.remove(ticker)
--- sma.py
+++ sma.py
@@ -2,13 +2,13 @@
 # This is a much simpler version of the strategy which is much more likely to generate a buy/sell signal
 def SMA(bought_stocks, sold_stocks, ticker, short_ma, long_ma):
     if short_ma > long_ma:
-        print(f"BUY {ticker} (SMA)")
+        print(f"BUY {ticker}   (SMA)")
         # Code to add/remove the stock from the list of bought/sold stocks
         bought_stocks.append(ticker)
         if ticker in sold_stocks:
             sold_stocks.remove(ticker)
     else:
-        print(f"SELL {ticker} (SMA)")
+        print(f"SELL {ticker}   (SMA)")
         bought_stocks.remove(ticker)
         if ticker in sold_stocks:
             sold_stocks.append(ticker)
