8c92e07947bdbf1fdc18e9a1ee66dbc6c1e6ebde
SYNewman <shmuelnewman@hotmail.com>
2024-11-10 14:05:05 +0000

    added line 1-2 comments to sma.py

--- sma.py
+++ sma.py
@@ -1,2 +1,4 @@
+# Code to run the SMA (simple moving average) strategy
+# This is a much simpler version of the strategy which is much more likely to generate a buy/sell signal
 def SMA():
     pass
