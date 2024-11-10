48851ac2f35a1a4f6b13fa287f25f48f669b2fb3
SYNewman <shmuelnewman@hotmail.com>
2024-11-10 13:46:21 +0000

    completed logic for moving averages strategy

--- moving_averages.py
+++ moving_averages.py
@@ -12,3 +12,9 @@ def moving_averages(bought_stocks, sold_stocks, ticker, price, short_ma, long_ma
             sold_stocks.append(ticker)
             if ticker in bought_stocks:
                 bought_stocks.remove(ticker)
+                
+    if ticker in bought_stocks:
+        if price < short_ma or short_ma < long_ma:
+            print(ticker, "SELL (moving averages)")
+            sold_stocks.append(ticker)
+            bought_stocks.remove(ticker)
