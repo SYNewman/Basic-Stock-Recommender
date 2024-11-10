c245ee784bf11a37b4707dec930dd22ce6e238f8
SYNewman <shmuelnewman@hotmail.com>
2024-11-10 13:37:13 +0000

    added bought and sold stocks to lists

--- moving_averages.py
+++ moving_averages.py
@@ -1,8 +1,10 @@
-def moving_averages(ticker, price, short_ma, long_ma):
+def moving_averages(bought_stocks, sold_stocks, ticker, price, short_ma, long_ma):
     if short_ma > long_ma:
         if price > short_ma:
             print(ticker, "BUY (moving averages)")
+            bought_stocks.append(ticker)
     
     elif short_ma < long_ma:
         if price < short_ma:
             print(ticker, "SELL (moving averages)")
+            sold_stocks.append(ticker)
