993b63a7ec5046136e567e568ccccc9b5d6cad37
SYNewman <shmuelnewman@hotmail.com>
2024-11-10 14:06:06 +0000

    created main logic of sma strategy

--- sma.py
+++ sma.py
@@ -1,4 +1,11 @@
 # Code to run the SMA (simple moving average) strategy
 # This is a much simpler version of the strategy which is much more likely to generate a buy/sell signal
-def SMA():
-    pass
+def SMA(short_ma, long_ma):
+    
+    # Code to generate a buy signal
+    if short_ma > long_ma:
+        print("BUY (SMA)")
+        
+    # Code to generate a sell signal
+    elif short_ma < long_ma:
+        print("SELL (SMA)")
