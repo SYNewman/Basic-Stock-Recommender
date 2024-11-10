affd993b587d0188883960ba88d6e1efd4019967
SYNewman <shmuelnewman@hotmail.com>
2024-11-10 14:55:14 +0000

    code in sma.py only removes stocks from lists they are already in

--- sma.py
+++ sma.py
@@ -7,13 +7,15 @@ def SMA(bought_stocks, sold_stocks, ticker, short_ma, long_ma):
         print("BUY (SMA)")
         # Code to add/remove the stock from the list of bought/sold stocks
         bought_stocks.append(ticker)
-        sold_stocks.remove(ticker)
+        if ticker in sold_stocks:
+            sold_stocks.remove(ticker)
         
     # Code to generate a sell signal
     elif short_ma < long_ma:
         print("SELL (SMA)")
         bought_stocks.remove(ticker)
-        sold_stocks.append(ticker)
+        if ticker in sold_stocks:
+            sold_stocks.append(ticker)
         
     # Code to run if neither a buy or sell signal is generated
     else:
